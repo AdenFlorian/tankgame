8a0d9418ea2647cf7ca01de66088ccd506a5e86f
David Valachovic <AdenFlorian@gmail.com>
2015-01-02 19:12:56 -0800

    added extension method

---
+++ Assets/_TanksGame/Scripts/Extensions/ExtTransform.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Extensions
+{
+    public static class ExtTransform
+    {
+        public static float PosX(this Transform transform)
+        {
+            return transform.position.x;
+        }
+    }
+}
