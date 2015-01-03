ff80fbd495c3bca1351a97d7975cccbc43499b48
David Valachovic <AdenFlorian@gmail.com>
2015-01-03 00:05:59 -0800

    cleanup and organization

---
+++ Assets/_TanksGame/Scripts/PrefabSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabSpawner : MonoBehaviour
+{
+    public GameObject prefabA;
+    public GameObject prefabB;
+    void Awake()
+    {
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
