ef8a2706542f682c31b43fb57db3fc045646c392
AdenFlorian <AdenFlorian@gmail.com>
2015-01-05 14:16:54 -0800

    Moving util scripts into zinke namespace and folder

---
+++ Assets/_ZinkeTools/Scripts/Editor/FBXScaleFix.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+namespace Zinke
+{
+    /// <summary>
+    /// For FBX models exported from blender
+    /// </summary>
+    public class FBXScaleFix : AssetPostprocessor
+    {
+        public void OnPreprocessModel()
+        {
+            ModelImporter modelImporter = (ModelImporter)assetImporter;
+            modelImporter.globalScale = 1;
+        }
+    }
+}
